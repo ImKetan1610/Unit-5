9b04b4e3407181bc9afef67e7b616ed9ba5ca319
ImKetan1610 <96099100+ImKetan1610@users.noreply.github.com>
2022-04-14 13:03:26 +0530

    Add files via upload

--- Sprint1/Day1/Assignment/src/index.js
+++ Sprint1/Day1/Assignment/src/index.js
@@ -1,1 +1,28 @@
+console.log("hello")
+import "./index.css";
+
+const main = document.getElementById("main");
+main.classList.add("main");
+const imageDiv = document.createElement("div");
+imageDiv.setAttribute("id","imgdiv")
+const logoIcon = document.createElement("img");
+logoIcon.src = "https://thumbs.dreamstime.com/b/taking-note-line-icon-vector-isolated-white-background-infographic-website-app-blue-designed-grid-system-100290741.jpg";
+logoIcon.setAttribute("width","50px")
+const heading = document.createElement("h3");
+heading.innerText = "Notes"
+const input = document.createElement("input");
+const button = document.createElement("button")
+button.innerText="Submit";
+button.addEventListener("click",handleNotes)
+imageDiv.append(logoIcon,heading,input,button)
+
+const output = document.createElement("ul")
+function handleNotes(){
+    let value = input.value;
+    const li = document.createElement("li")
+    li.innerText = value;
+    output.append(li)
+}
+
+main.append(imageDiv,output)
 
