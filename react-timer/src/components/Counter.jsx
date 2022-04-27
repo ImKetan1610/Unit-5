b76c9c50b47b268e2dba81dd6f0f16611835ddf1
Ketan Makode <ketan.makode@gmail.com>
2022-04-27 23:15:24 +0530

    Add files via upload

--- react-timer/src/components/Counter.jsx
+++ react-timer/src/components/Counter.jsx
@@ -1,1 +1,34 @@
+import { useEffect, useState } from "react";
+
+export const Counter = () => {
+ const [ counter, setCounter ] = useState(0);
+//  const [ end, setEnd ] = useState(1);
+
+
+ useEffect(() => {
+     let countDown = setInterval(() => {
+         setCounter((prevCount) => {
+             if (prevCount + 1 == 60 ) {
+                 clearInterval(countDown)
+             }
+             return prevCount + 1;
+         })
+     }, 1000)
+     return () => {
+         clearInterval(countDown);
+     }
+ }, [])
+
+
+
+ return (
+     <div>
+        <div className="Timer">Timer: {counter} sec </div>
+        <hr />
+        {/* <input type= "text" className="StopTimer" onChange={(e) => setEnd(e.target.value) } />&nbsp;
+        
+        <button onClick={handleEnd}>Stop At</button> */}
+     </div>
+ )
+}
 
