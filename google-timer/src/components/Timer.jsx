1d047bcca68fd4527428b78e0262e64fbb187131
Ketan Makode <ketan.makode@gmail.com>
2022-05-01 15:10:09 +0530

    Add files via upload

--- google-timer/src/components/Timer.jsx
+++ google-timer/src/components/Timer.jsx
@@ -1,1 +1,36 @@
+import { useEffect, useRef, useState } from 'react';
 
+export const Timer = () => {
+    const [ time, setTime ] = useState(0)
+    const ref= useRef(null);
+    useEffect(() => {
+        start()
+    },[])
+
+    const start = () => {
+        clearInterval(ref.current)
+        ref.current = setInterval(()=>{
+            setTime((preValue)=>{
+                return preValue+1;
+            })
+        }, 1000);
+    }
+
+    const pause = () =>{
+        clearInterval(ref.current)
+    }
+
+    const stop = () =>{
+        setTime(0)
+    }
+
+
+    return (
+        <div className='timerDiv'>
+            <h1>Timer: {time<10 ? 0+time : time} seconds</h1>
+            <button onClick={pause}>⏸️</button>
+            <button onClick={stop}>🔃</button>
+            <button onClick={start}>▶️</button>
+        </div>
+    ) 
+}
